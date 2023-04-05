64332f522376c5c6162a7204614ee535df3dc6dc
EmmaG2 <itsmeemmadev@gmail.com>
2023-04-05 14:35:07 -0600

    second setup

--- basic_template.cpp
+++ basic_template.cpp
@@ -2,14 +2,13 @@
 
 using namespace std;
 
+#define ln "\n"
+#define ll long long
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
-	cin >> n;
-
-	cout << n << "\n";
 
 	return 0;
 }
--- src/2016/test_2016.cpp
+++ src/2016/test_2016.cpp
@@ -2,14 +2,32 @@
 
 using namespace std;
 
+#define ln "\n"
+#define ll long long
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
+	cout.tie(NULL);
+
+	ll n;
 	cin >> n;
 
-	cout << n << "\n";
+	while (n != 1)
+	{
+		cout << n << ' ';
+		if (n & 1)
+		{
+			n = 3 * n + 1;
+		}
+		else
+		{
+			n = n / 2;
+		}
+	}
+
+	cout << n << ln;
 
 	return 0;
 }
