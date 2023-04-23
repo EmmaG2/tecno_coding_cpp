772b1ad67475fd6ebda168fdcebc1fe72b7d691a
EmmaG2 <itsmeemmadev@gmail.com>
2023-04-23 02:25:58 -0600

    [omegaup] more problems

---
+++ src/lib/omega_up/par_impar.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ln "\n"
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin >> n;
+
+	cout << (n & 1 ? "impar" : "par") << ln;
+	return 0;
+}
