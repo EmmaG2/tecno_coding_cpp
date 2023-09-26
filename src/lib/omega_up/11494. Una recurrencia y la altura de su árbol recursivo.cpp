5808b9ffe8fb35db24b53137d1e80675d17aaa58
EmmaG2 <itsmeemmadev@gmail.com>
2023-09-26 10:15:06 -0600

    ejercicios de recursividad

---
+++ src/lib/omega_up/11494. Una recurrencia y la altura de su árbol recursivo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int heigth(int root)
+{
+    if (root <= 5) return 1;
+    return 1 + max(heigth(root - 5), heigth(floor(3 * root / 4)));
+}
+
+int fn(int n)
+{
+    if (n <= 5 ) return 1;
+    return fn(n - 5) + fn(floor(3 * n / 4));
+}
+
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n;
+
+    cin >> n;
+
+    cout << fn(n) << ' ' << heigth(n) << endl;
+    return 0;
+}
